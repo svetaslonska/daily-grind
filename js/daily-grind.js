01b1afc4cffd725e72108a1d036d9d9ff00f4b9d
Svitlana Slonska <svetaslonska@gmail.com>
2023-06-05 15:11:39 -0700

    update changes

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -11,9 +11,34 @@ let myDate = new Date();
 
 let today = myDate.getDay();
 
+let coffee = "";
+
+function coffeeTemplate(coffee){
+
+    return `
+    <p>
+            <img src="${coffee.pic}" alt="${coffee.alt}" id = "coffee"/>
+            <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}</p>
+    `;
+
+}
+
 //test today = 3;
 
 switch(today){
+
+    case 1:
+        today = "Monday";
+
+        coffee = {
+            name: "Bubble Tea",
+            pic: "images/bubble-tea.jpg",
+            alt: "A picture of a bubble tea",
+            color: "pink",
+            day: "Monday",
+            desc: `I love me some bubble tea!`
+        };
+    break;
 
     case 2:
         today = "Tuesday";
@@ -27,7 +52,7 @@ switch(today){
         today ="Day is unknown!";
 }
 
-let coffee = {
+coffee = {
     name: "Bubble Tea",
     pic: "images/bubble-tea.jpg",
     alt: "A picture of a bubble tea",
@@ -35,5 +60,7 @@ let coffee = {
     day: "Wednesday",
     desc: `I love me some bubble tea!`
 }
+
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
 
 console.log(coffee);
