9bf88c9a9e7f6aef3dfba6fb2feca3658a1c222d
Svitlana Slonska <svetaslonska@gmail.com>
2023-06-06 23:08:56 -0700

    update changes

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -40,10 +40,22 @@ if(urlParams.has("day")){//from querystring
  }
 
  today = parseInt(today);
- 
 
 switch(today){
+//here we created case with Sunday cold brew special
+    case 0:
+        today = "Sunday";
 
+        coffee = {
+            name: "Cold brew",
+            pic: "images/cold-brew.jpg",
+            alt: "A picture of cold brew",
+            color: "chocolate",
+            day: "Sunday",
+            desc: `I love cold brew in a hot summer`
+        };
+    break;
+//here we created case with Monday bubble tea special
     case 1:
         today = "Monday";
 
@@ -56,7 +68,7 @@ switch(today){
             desc: `I love me some bubble tea!`
         };
     break;
-
+//here we created case with Tuesday mocha special
     case 2:
         today = "Tuesday";
 
@@ -69,9 +81,55 @@ switch(today){
             desc: `I like mocha!`
         };
     break;
-
+//here we created case with Wednesday caramel late special
     case 3:
         today = "Wednesday";
+
+        coffee = {
+            name: "Caramel Latte",
+            pic: "images/caramel-latte.jpg",
+            alt: "A picture of caramel latte",
+            color: "coral",
+            day: "Wednesday",
+            desc: `I love caramel latte!`
+        };
+    break;
+//here we created case with Thursday frappaccino special
+    case 4:
+        today = "Thursday";
+
+        coffee = {
+            name: "Frappaccino",
+            pic: "images/frappaccino.jpg",
+            alt: "A picture of frappaccino",
+            color: "tan",
+            day: "Thursday",
+            desc: `Can't live without frappaccino!`
+        };
+    break;
+    case 5:
+        today = "Friday";
+//here we created case with Friday pumpkin spice late special
+        coffee = {
+            name: "Pumpkin spice latte",
+            pic: "images/pumpkin-spice-latte.jpg",
+            alt: "A picture of pumpkin spice latte",
+            color: "orange",
+            day: "Friday",
+            desc: `I like a smell of pumpkin spice taste!`
+            };
+    break;
+    case 6:
+        today = "Saturday";
+//here we created case with Saturday drip special       
+        coffee = {
+            name: "Drip",
+            pic: "images/drip.jpg",
+            alt: "A picture of drip",
+            color: "steelblue",
+            day: "Saturday",
+            desc: `The drip is so great!`
+        };
     break;
 
     default:
